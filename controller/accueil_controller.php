d15796df1f61a087537ffdfe2980b21ac60d8fcf
Esdras <eonionkiton@gmail.com>
2025-06-12 21:49:54 +0200

    correction accueil controller

--- controller/accueil_controller.php
+++ controller/accueil_controller.php
@@ -2,7 +2,7 @@
 
 function index() : void {
     $title = "Accueil";
-    $desc = "Page d'accueil du site  !";
+    $desc = "Page d'accueil du site  ";
     require_once '/view/partials/header.php';
     require_once 'view/accueil_view.php';
     require_once '/view/partials/footer.php';
