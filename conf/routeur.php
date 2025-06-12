1067f4736421ad10824527385aff5b64fd26d736
Florian Esdras O. <86320761+Esdro@users.noreply.github.com>
2025-06-12 18:14:51 +0000

    Ajout de la logique de routage et des contrôleurs pour la gestion des requêtes

---
+++ conf/routeur.php
@@ -0,0 +1,33 @@
+<?php
+
+
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Retirer le slash initial et final
+
+$url = trim($url, '/');
+
+// Diviser l'URL en segments
+$items = explode('/', $url);
+
+// Obtenir le contrôleur et l'action à partir de l'URL
+$controller = !empty($items[0]) ? $items[0] : 'accueil';
+$action = !empty($items[1]) ? $items[1] : 'index';
+
+// vérifier l'action 2 
+if (isset($items[2]) && !empty($items[2])) {
+    $action = $items[2];
+}
+
+// Inclure le fichier de contrôleur approprié
+$controllerFile = __DIR__ . '/controller/' . $controller . '_controller.php';
+if (file_exists($controllerFile)) {
+    include $controllerFile;
+    if (function_exists($action)) {
+        call_user_func($action);
+    } else {
+        include __DIR__ . '/controller/error_404.php';
+    }
+} else {
+    include __DIR__ . '/controller/error_404.php';
+}
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// mettre la date et l'heure par défaut
+date_default_timezone_set('Europe/Paris');
+// Démarrer la session
+session_start();
+
+// mettre en place un cookie de session avec une durée de vie de 1h avec le nom 'sae202_id'
+setcookie('sae202_id', session_id(), time() + 3600, '/');
+
+
+
+require_once __DIR__ . '/conf/routeur.php';
